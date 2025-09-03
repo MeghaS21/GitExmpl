5b7425e91a3b4695441e45fb234f96e7fced7a56
MeghaS21 <yosamhere@gmail.com>
2025-09-02 22:21:17 -0400

    first commit

---
+++ src/main/java/STAD/GitExmpl/App.java
@@ -0,0 +1,13 @@
+package STAD.GitExmpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/STAD/GitExmpl/Test.java
@@ -0,0 +1,10 @@
+package STAD.GitExmpl;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
